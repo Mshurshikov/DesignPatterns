79cffdbd1e2a17b8b3427cc6f4b919ef017efb0d
m.shurshikov@gmail.com <shurshikov@MWLHP002944.mewa-group.com>
2018-04-14 10:31:12 +0200

    Constructor implementation

--- DesignPatterns/src/pattern/observer/javaobserver/WeatherData.java
+++ DesignPatterns/src/pattern/observer/javaobserver/WeatherData.java
@@ -3,5 +3,7 @@ package pattern.observer.javaobserver;
 import java.util.Observable;
 
 public class WeatherData extends Observable {
-
+	public WeatherData() {
+		// TODO Auto-generated constructor stub
+	}
 }
