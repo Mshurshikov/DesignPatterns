6b6ce45e4e2c1d1b8be40823ea2c364a6554a5c5
m.shurshikov@gmail.com <shurshikov@MWLHP002944.mewa-group.com>
2018-04-14 10:50:18 +0200

    Initial class structure created

--- DesignPatterns/src/pattern/observer/javaobserver/WeatherData.java
+++ DesignPatterns/src/pattern/observer/javaobserver/WeatherData.java
@@ -3,7 +3,32 @@ package pattern.observer.javaobserver;
 import java.util.Observable;
 
 public class WeatherData extends Observable {
+	
+	private float temperature;
+	private float humidity;
+	private float pressure;
+	
 	public WeatherData() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void measurementsChanged() {
+		
+	}
+	
+	public void setMeasurements(float temperature, float humidity, float pressure) {
+		
+	}
+	
+	public float getTemperature() {
+		return temperature;
+	}
+	
+	public float getHumidity() {
+		return humidity;
+	}
+	
+	public float getPressure() {
+		return pressure;
 	}
 }
