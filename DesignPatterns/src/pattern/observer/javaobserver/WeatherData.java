67e32ece90674bd85798c306d4cbd0fdb3bfa504
m.shurshikov@gmail.com <shurshikov@MWLHP002944.mewa-group.com>
2018-04-17 21:46:25 +0200

    WeatherData reworked

--- DesignPatterns/src/pattern/observer/javaobserver/WeatherData.java
+++ DesignPatterns/src/pattern/observer/javaobserver/WeatherData.java
@@ -13,11 +13,16 @@ public class WeatherData extends Observable {
 	}
 	
 	public void measurementsChanged() {
-		
+		setChanged();
+		notifyObservers();
 	}
 	
 	public void setMeasurements(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		this.pressure = pressure;
 		
+		measurementsChanged();
 	}
 	
 	public float getTemperature() {
